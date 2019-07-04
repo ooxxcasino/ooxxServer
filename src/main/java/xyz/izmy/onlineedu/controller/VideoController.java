b378087f706b5e1441b0abab2805b3dac5c91679
seedzy <13208713700@163.com>
2019-07-04 12:09:25 +0800

    添加部分内容

--- src/main/java/xyz/izmy/onlineedu/controller/VideoController.java
+++ src/main/java/xyz/izmy/onlineedu/controller/VideoController.java
@@ -2,10 +2,7 @@ package xyz.izmy.onlineedu.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import xyz.izmy.onlineedu.entity.Video;
 import xyz.izmy.onlineedu.repository.VideoRepository;
 
@@ -31,9 +28,27 @@ public class VideoController {
         return video;
     }
 
+
     @GetMapping(value = "/{id}")
     public Object getVideoById(@PathVariable("id") Long id){
-       return videoRepository.findById(id);
+
+        return videoRepository.findById(id);
+    }
+
+    @PostMapping(value = "/add")
+    public Object videoAdd(/*@RequestParam("video_image_url") String video_image_url
+            ,@RequestParam("video_info") String video_info,@RequestParam("video_name") String video_name
+            ,@RequestParam("video_price") int video_price,@RequestParam("video_type") String video_type
+            ,@RequestParam("video_url") String video_url*/@RequestBody Video video){
+        /*Video video=new Video();
+        video.setName(video_name);
+        video.setInfo(video_info);
+        video.setImageUrl(video_image_url);
+        video.setUrl(video_url);
+        video.setType(video_type);
+        video.setPrice(video_price);*/
+
+        return videoRepository.save(video);
     }
 
 }
--- src/main/java/xyz/izmy/onlineedu/repository/VideoRepository.java
+++ src/main/java/xyz/izmy/onlineedu/repository/VideoRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import xyz.izmy.onlineedu.entity.Video;
 
 public interface VideoRepository extends JpaRepository<Video,Long> {
+
+
 }
